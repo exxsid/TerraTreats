dd0b5774bd5591b64bb89abc56c61d6ee6773dc2
exxsid <cortezleoanthony5@gmail.com>
2024-03-24 20:52:42 +0800

    add app theme

---
+++ lib/util/app_theme.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static const Color primary = Color(0x004f6f52);
+  static const Color highlight = Color(0x00f7f7f7);
+  static const Color secondary = Color(0x0086A789);
+}
