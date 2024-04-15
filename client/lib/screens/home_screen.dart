6c5409ec1fc292dc4cddb7e5cf068c595487a5dc
exxsid <cortezleoanthony5@gmail.com>
2024-04-15 10:37:56 +0800

    migrate home screen to consumer widget

--- client/lib/screens/home_screen.dart
+++ client/lib/screens/home_screen.dart
@@ -1,17 +1,24 @@
 import "package:flutter/material.dart";
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class Home extends StatefulWidget {
+class Home extends ConsumerStatefulWidget {
   const Home({super.key});
 
   @override
-  State<Home> createState() => _HomeState();
+  _HomeState createState() => _HomeState();
 }
 
-class _HomeState extends State<Home> {
+class _HomeState extends ConsumerState<Home> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text("Home screen"),
+      child: Center(
+        child: Column(
+          children: [
+            Text("Home screen"),
+          ],
+        ),
+      ),
     );
   }
 }
