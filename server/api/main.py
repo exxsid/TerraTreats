8f37977cbe588e511f726fe1eae056e7ce3bf08f
exxsid <cortezleoanthony5@gmail.com>
2024-04-16 18:50:03 +0800

    changed login request method

--- server/api/main.py
+++ server/api/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Response
 from fastapi.responses import JSONResponse
+import json
 
 from utils import authentication as auth
 from models.api_base_model import Login
@@ -12,13 +13,13 @@ def get_users():
     return auth.get_users()
 
 
-@app.get('/login')
+@app.post('/login')
 async def user_login(credentials: Login):
     result = await auth.login_user(credentials)
     if result is None:
         return None
 
-    content = {"message": "yehey"}
+    content = {"email": result.email, "password": result.password}
     response = JSONResponse(content=content)
     response.set_cookie(key="user_id", value=result.id, secure=True)
     return response
