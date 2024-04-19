8543069624986c6f1ed3fb80256c518d05231ef8
exxsid <cortezleoanthony5@gmail.com>
2024-04-19 20:38:07 +0800

    cleaned the code for selected product screen

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -39,6 +39,6 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Login();
+    return const BottomNavBar();
   }
 }
