c331c4c3e9b091b5abd08f923c7d646244a25e15
exxsid <cortezleoanthony5@gmail.com>
2024-04-30 23:13:59 +0800

    add the search functionality

---
+++ client/lib/services/search_service.dart
@@ -0,0 +1,19 @@
+import "package:terratreats/models/product_model.dart";
+import "dart:convert";
+import "package:http/http.dart" as http;
+import "package:flutter_dotenv/flutter_dotenv.dart";
+
+final baseUrl = dotenv.env['BASE_URL'];
+
+Future<List<dynamic>> getSearchedProducts(String searchString) async {
+  final uri = Uri.parse("$baseUrl/search?search_str=$searchString");
+
+  final response = await http.get(uri);
+
+  if (response.statusCode == 200) {
+    final data = jsonDecode(response.body) as List<dynamic>;
+    return data.map((item) => Product.fromJson(item)).toList();
+  } else {
+    throw Exception("Failed to get recommended products.");
+  }
+}
