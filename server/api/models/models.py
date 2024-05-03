b20d3d2028f6f4fe86395d9a3d382656ae17b8e9
exxsid <cortezleoanthony5@gmail.com>
2024-05-03 18:07:49 +0800

    add sold column in products table

--- server/api/models/models.py
+++ server/api/models/models.py
@@ -87,6 +87,7 @@ class Product(Base):
     unit: Mapped[str] = mapped_column(String())
     image_url: Mapped[str] = mapped_column(String())
     rating: Mapped[float] = mapped_column(Float(), default=0, nullable=True)
+    sold: Mapped[int] = mapped_column(Integer(), default=0, nullable=True)
     category_id: Mapped[int] = mapped_column(
         ForeignKey('categories.category_id'))
     seller_id: Mapped[int] = mapped_column(ForeignKey(
