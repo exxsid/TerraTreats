3035238dc823aee9e245a0f30995fe09e115c6fd
exxsid <cortezleoanthony5@gmail.com>
2024-05-16 10:53:05 +0800

    edit the implementation fo adding review

--- server/api/utils/review_util.py
+++ server/api/utils/review_util.py
@@ -5,7 +5,7 @@ from sqlalchemy import func
 from sqlalchemy.exc import IntegrityError
 import uuid
 
-from models.models import Order, Review, User
+from models.models import Order, Product, Review, User
 from models.api_base_model import Review as rev
 
 engine = engine = create_engine('postgresql+psycopg://leo:1234@127.0.0.1:5432/terratreats',
@@ -50,14 +50,33 @@ async def add_review(new_review: rev):
             session.add(review)
             session.flush()
             
-            # TODO updating order status 
-            query = update(Order).\
+            # get the current rating of the product
+            current_rating_query = select(Product.rating).\
+                select_from(Product).\
+                filter(Product.product_id == new_review.product_id)
+            current_rating = session.execute(current_rating_query).all()
+            
+            print("HHHHHHHAAAAA",current_rating)
+            
+            updated_rating = await calculate_rating(
+                new_rating=new_review.rating, 
+                current_rating=current_rating[0][0]
+                )
+            
+            # update the row with the updated rating
+            update_rating_query = update(Product).\
+                where(Product.product_id == new_review.product_id).\
+                values(rating=updated_rating)
+            update_rating_result = session.execute(update_rating_query)
+            
+            # update the order status of the row
+            orderstatus_query = update(Order).\
                 where(Order.order_id == new_review.order_id).\
                 values(order_status = 'reviewed')
+            orderstatus_result = session.execute(orderstatus_query)
                 
-            result = session.execute(query)
-                
-            if result.rowcount <= 0:
+            if orderstatus_result.rowcount <= 0 or \
+                update_rating_result.rowcount <= 0:
                 return False
             
             session.commit()
@@ -68,3 +87,9 @@ async def add_review(new_review: rev):
         print("EEEEEEEEEEEEEEE", e)
         session.rollback()
         return False
+    
+    
+async def calculate_rating(new_rating: float, current_rating: float):
+    if current_rating == 0:
+        return new_rating
+    return (current_rating + new_rating) / 2
