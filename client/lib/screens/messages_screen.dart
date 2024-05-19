44d69340f61e38777e11af725fd783bc404f3dfc
exxsid <cortezleoanthony5@gmail.com>
2024-05-19 17:40:00 +0800

    add referesh feature for messages

--- client/lib/screens/messages_screen.dart
+++ client/lib/screens/messages_screen.dart
@@ -20,104 +20,113 @@ class _MessagesState extends State<Messages> {
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.all(8),
-      child: FutureBuilder(
-        future: getChats(),
-        builder: (context, snapshot) {
-          if (!snapshot.hasData) {
-            return LoadingIndicator.circularLoader();
-          }
-
-          if (snapshot.hasError) {
-            return Container(
-              child: const Center(
-                child: Text("Can't load messages."),
-              ),
-            );
-          }
-
-          if (snapshot.data!.isEmpty) {
-            return Container(
-              child: const Center(
-                child: Text("No messages."),
-              ),
-            );
-          }
-
-          final messages = snapshot.data!;
-
-          return ListView.builder(
-            itemCount: messages.length,
-            itemBuilder: (context, index) {
-              final message = messages[index];
-              return Consumer(
-                builder: (context, ref, child) {
-                  return InkWell(
-                    child: Container(
-                      margin: const EdgeInsets.only(bottom: 8),
-                      padding: const EdgeInsets.all(8),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(12),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.blueGrey,
-                            blurRadius: 2,
-                            spreadRadius: 0.5,
-                          ),
-                        ],
-                        color: AppTheme.highlight,
-                      ),
-                      child: Row(
-                        children: [
-                          Container(
-                            width: 50,
-                            height: 50,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(50),
-                              color: Colors.blueAccent,
-                            ),
-                            child: Icon(
-                              Ionicons.person_outline,
-                              color: Colors.white,
-                            ),
-                          ),
-                          const SizedBox(
-                            width: 16,
-                          ),
-                          Text(
-                            message['recipient'],
-                            style: TextStyle(
-                              color: AppTheme.primary,
-                              fontSize: 16,
-                              fontWeight: FontWeight.w500,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    onTap: () {
-                      ref
-                          .read(chatChangeNotifierProvider.notifier)
-                          .updateRecipient(message['recipient']);
-                      ref
-                          .read(chatChangeNotifierProvider.notifier)
-                          .updateChatId(message['id']);
-
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) {
-                            return ChatScreen();
-                          },
-                        ),
-                      );
-                    },
-                  );
-                },
-              );
-            },
-          );
+      child: RefreshIndicator(
+        child: messagesListView(),
+        onRefresh: () async {
+          setState(() {});
         },
       ),
+    );
+  }
+
+  FutureBuilder<List<Map<String, dynamic>>> messagesListView() {
+    return FutureBuilder(
+      future: getChats(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return LoadingIndicator.circularLoader();
+        }
+
+        if (snapshot.hasError) {
+          return Container(
+            child: const Center(
+              child: Text("Can't load messages."),
+            ),
+          );
+        }
+
+        if (snapshot.data!.isEmpty) {
+          return Container(
+            child: const Center(
+              child: Text("No messages."),
+            ),
+          );
+        }
+
+        final messages = snapshot.data!;
+
+        return ListView.builder(
+          itemCount: messages.length,
+          itemBuilder: (context, index) {
+            final message = messages[index];
+            return Consumer(
+              builder: (context, ref, child) {
+                return InkWell(
+                  child: Container(
+                    margin: const EdgeInsets.only(bottom: 8),
+                    padding: const EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.blueGrey,
+                          blurRadius: 2,
+                          spreadRadius: 0.5,
+                        ),
+                      ],
+                      color: AppTheme.highlight,
+                    ),
+                    child: Row(
+                      children: [
+                        Container(
+                          width: 50,
+                          height: 50,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(50),
+                            color: Colors.blueAccent,
+                          ),
+                          child: Icon(
+                            Ionicons.person_outline,
+                            color: Colors.white,
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 16,
+                        ),
+                        Text(
+                          message['recipient'],
+                          style: TextStyle(
+                            color: AppTheme.primary,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  onTap: () {
+                    ref
+                        .read(chatChangeNotifierProvider.notifier)
+                        .updateRecipient(message['recipient']);
+                    ref
+                        .read(chatChangeNotifierProvider.notifier)
+                        .updateChatId(message['id']);
+
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) {
+                          return ChatScreen();
+                        },
+                      ),
+                    );
+                  },
+                );
+              },
+            );
+          },
+        );
+      },
     );
   }
 }
