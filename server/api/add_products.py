9a91ff0350545df72230f87b3bebe8eede6bc88d
exxsid <cortezleoanthony5@gmail.com>
2024-05-23 14:54:06 +0800

    file to add products

---
+++ server/api/add_products.py
@@ -0,0 +1,492 @@
+products = [
+    {
+        "id": 5,
+        "delivery": [
+            "8:00 AM - 9:00 AM",
+        ],
+        "products": [
+            {
+                "product_name": "Baguio Strawberries",
+                "description": "Fresh from Baguio City",
+                "price": 25.0,
+                "stock": 10,
+                "unit": "kilo",
+                "image": 0,
+                "shipping_fee": 5,
+                "category_id": 1,
+            },
+            {
+                "product_name": "Bagnet",
+                "description": "Crispy at masarap na bagnet ba ang hanap? bili na samin.",
+                "price": 100,
+                "stock": 10,
+                "unit": "kilo",
+                "image": 1,
+                "shipping_fee": 5,
+                "category_id": 6,
+            },
+            {
+                "product_name": "Dried Mango",
+                "description": "Dried native mango",
+                "price": 50,
+                "stock": 10,
+                "unit": "pc",
+                "image": 2,
+                "shipping_fee": 5,
+                "category_id": 6,
+            },
+            {
+                "product_name": "Fresh Native Mango",
+                "description": "Bagong pitas na native na manga galing sa bakuran",
+                "price": 25,
+                "stock": 15,
+                "unit": "kilo",
+                "image": 3,
+                "shipping_fee": 5,
+                "category_id": 1,
+            },
+            {
+                "product_name": "Pechay",
+                "description": "Laking bakuran na pechay. Hindi naisprayan",
+                "price": 15,
+                "stock": 20,
+                "unit": "pc",
+                "image": 4,
+                "shipping_fee": 5,
+                "category_id": 2,
+            },
+        ],
+    },
+    {
+        "id": 6,
+        "delivery": [
+            "8:00 AM - 9:00 AM",
+        ],
+        "products": [
+            {
+                "product_name": "Embutido",
+                "description": "Masarap na embitudo for everyone",
+                "price": 75,
+                "stock": 20,
+                "unit": "pc",
+                "image": 0,
+                "shipping_fee": 0,
+                "category_id": 6,
+            },
+            {
+                "product_name": "Sinandomeng Rice | 25kg",
+                "description": "Sinandomeng primium Well-Milled Rice",
+                "price": 300,
+                "stock": 20,
+                "unit": "sack",
+                "image": 1,
+                "shipping_fee": 10,
+                "category_id": 4,
+            },
+            {
+                "product_name": "Kesong Puti",
+                "description": "Kesong Puti o carabao cheese. Puno ng bitamina",
+                "price": 25,
+                "stock": 5,
+                "unit": "pc",
+                "image": 2,
+                "shipping_fee": 0,
+                "category_id": 5,
+            },
+        ],
+    },
+    {
+        "id": 7,
+        "delivery": [
+            "8:00 AM - 9:00 AM",
+        ],
+        "products": [
+            {
+                "product_name": "Bagoong",
+                "description": "Homemade bagoong",
+                "price": 20,
+                "stock": 45,
+                "unit": "liter",
+                "image": 0,
+                "shipping_fee": 0,
+                "category_id": 6,
+            },
+            {
+                "product_name": "Native Chicken",
+                "description": "Free-range chickens known for their flavorful meat, ideal for traditional Filipino dishes like tinola or adobo.",
+                "price": 50,
+                "stock": 15,
+                "unit": "kilo",
+                "image": 1,
+                "shipping_fee": 5,
+                "category_id": 3,
+            },
+            {
+                "product_name": "Pork Chop",
+                "description": "Backyard raised pig, haan nga nataba",
+                "price": 70,
+                "stock": 10,
+                "unit": "kilo",
+                "image": 2,
+                "shipping_fee": 0,
+                "category_id": 3,
+            },
+        ],
+    },
+    {
+        "id": 8,
+        "delivery": [
+            "8:00 AM - 9:00 AM",
+        ],
+        "products": [
+            {
+                "product_name": "Skinless Longganisa",
+                "description": "Homemade longanisa. hindi matamis",
+                "price": 50,
+                "stock": 20,
+                "unit": "pc",
+                "image": 0,
+                "shipping_fee": 5,
+                "category_id": 6,
+            },
+            {
+                "product_name": "Native Bawang",
+                "description": "Fresh bawang galing Ilocos",
+                "price": 40,
+                "stock": 20,
+                "unit": "kilo",
+                "image": 1,
+                "shipping_fee": 5,
+                "category_id": 2,
+            },
+            {
+                "product_name": "Sibuyas",
+                "description": "Masarap na panggisa, nananam",
+                "price": 20,
+                "stock": 30,
+                "unit": "kilo",
+                "image": 2,
+                "shipping_fee": 0,
+                "category_id": 2,
+            },
+        ],
+    },
+    {
+        "id": 9,
+        "delivery": [
+            "8:00 AM - 9:00 AM",
+        ],
+        "products": [
+            {
+                "product_name": "Patatas",
+                "description": "Darakkel nga patatas",
+                "price": 50,
+                "stock": 20,
+                "unit": "kilo",
+                "image": 0,
+                "shipping_fee": 5,
+                "category_id": 2,
+            },
+            {
+                "product_name": "Jasmine Rice",
+                "description": "Fresh jasmine rice. well-milled",
+                "price": 25,
+                "stock": 50,
+                "unit": "kilo",
+                "image": 1,
+                "shipping_fee": 5,
+                "category_id": 4,
+            },
+            {
+                "product_name": "Saging na saba",
+                "description": "Fresh saging. laking bakuran",
+                "price": 30,
+                "stock": 10,
+                "unit": "kilo",
+                "image": 2,
+                "shipping_fee": 5,
+                "category_id": 1,
+            },
+        ],
+    },
+    {
+        "id": 10,
+        "delivery": [
+            "8:00 AM - 9:00 AM",
+        ],
+        "products": [
+            {
+                "product_name": "Native na manok",
+                "description": "Hindi laking feeds, natural",
+                "price": 50,
+                "stock": 10,
+                "unit": "kilo",
+                "image": 0,
+                "shipping_fee": 0,
+                "category_id": 3,
+            },
+            {
+                "product_name": "Kambing meat",
+                "description": "Naimas nga kalding",
+                "price": 60,
+                "stock": 5,
+                "unit": "kilo",
+                "image": 1,
+                "shipping_fee": 5,
+                "category_id": 3,
+            },
+            {
+                "product_name": "Baka | Beef",
+                "description": "dakkel ken naimas",
+                "price": 100,
+                "stock": 5,
+                "unit": "kilo",
+                "image": 2,
+                "shipping_fee": 5,
+                "category_id": 3,
+            },
+        ],
+    },
+    {
+        "id": 11,
+        "delivery": [
+            "8:00 AM - 9:00 AM",
+        ],
+        "products": [
+            {
+                "product_name": "Goat Cheese",
+                "description": "Fresh kambing cheese",
+                "price": 50,
+                "stock": 5,
+                "unit": "pc",
+                "image": 0,
+                "shipping_fee": 5,
+                "category_id": 6,
+            },
+            {
+                "product_name": "Daing na pusit",
+                "description": "Nananam nga daing nga pusit",
+                "price": 30,
+                "stock": 20,
+                "unit": "pack",
+                "image": 1,
+                "shipping_fee": 5,
+                "category_id": 6,
+            },
+            {
+                "product_name": "Narvacan Bagnet",
+                "description": "Bagnet galing narvacan. Naimas",
+                "price": 50,
+                "stock": 10,
+                "unit": "pc",
+                "image": 2,
+                "shipping_fee": 5,
+                "category_id": 6,
+            },
+        ],
+    },
+    {
+        "id": 12,
+        "delivery": [
+            "8:00 AM - 9:00 AM",
+        ],
+        "products": [
+            {
+                "product_name": "Bawang",
+                "description": "Fresh bawang",
+                "price": 40,
+                "stock": 20,
+                "unit": "kilo",
+                "image": 0,
+                "shipping_fee": 5,
+                "category_id": 2,
+            },
+            {
+                "product_name": "Pulang sibuyas",
+                "description": "pulang sibuyas galing nueva ecija",
+                "price": 30,
+                "stock": 20,
+                "unit": "kilo",
+                "image": 1,
+                "shipping_fee": 5,
+                "category_id": 2,
+            },
+            {
+                "product_name": "Kambing",
+                "description": "masarap na kambing",
+                "price": 75,
+                "stock": 5,
+                "unit": "kilo",
+                "image": 2,
+                "shipping_fee": 5,
+                "category_id": 3,
+            },
+        ],
+    },
+    {
+        "id": 13,
+        "delivery": [
+            "8:00 AM - 9:00 AM",
+        ],
+        "products": [
+            {
+                "product_name": "Baboy buto-buto",
+                "description": "Naimas nga tulang ti baboy",
+                "price": 90,
+                "stock": 10,
+                "unit": "kilo",
+                "image": 0,
+                "shipping_fee": 10,
+                "category_id": 3,
+            },
+            {
+                "product_name": "Gabi",
+                "description": "masarap na gabi",
+                "price": 27,
+                "stock": 20,
+                "unit": "kilo",
+                "image": 1,
+                "shipping_fee": 5,
+                "category_id": 2,
+            },
+            {
+                "product_name": "Talbos ng kamote",
+                "description": "fresh na fresh na talbos ng kamote, di naisprayan",
+                "price": 20,
+                "stock": 20,
+                "unit": "pc",
+                "image": 2,
+                "shipping_fee": 5,
+                "category_id": 2,
+            },
+        ],
+    },
+    {
+        "id": 14,
+        "delivery": [
+            "8:00 AM - 9:00 AM",
+        ],
+        "products": [
+            {
+                "product_name": "Longganisa",
+                "description": "Homemade longanisa. Siguradong masarap. Baboy",
+                "price": 30,
+                "stock": 20,
+                "unit": "kilo",
+                "image": 0,
+                "shipping_fee": 0,
+                "category_id": 6,
+            },
+            {
+                "product_name": "Pork Tocino",
+                "description": "Homemade Pork tocino. Sakto timpla kaya champion ka",
+                "price": 35,
+                "stock": 20,
+                "unit": "kilo",
+                "image": 1,
+                "shipping_fee": 0,
+                "category_id": 6,
+            },
+            {
+                "product_name": "Tinapa",
+                "description": "Masarap na tinapa para sa lahat",
+                "price": 30,
+                "stock": 20,
+                "unit": "pack",
+                "image": 2,
+                "shipping_fee": 0,
+                "category_id": 6,
+            },
+        ],
+    },
+]
+
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+
+from models.models import Product, DeliverySchedule
+from models.api_base_model import Login, Signup
+
+from dotenv import load_dotenv, dotenv_values
+import os
+import cloudinary
+from cloudinary.uploader import upload
+
+engine = engine = create_engine(
+    "postgresql+psycopg://leo:1234@127.0.0.1:5432/terratreats", echo=True
+)
+
+load_dotenv()
+cloudinary.config(
+    cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"),
+    api_key=os.getenv("CLOUDINARY_API_KEY"),
+    api_secret=os.getenv("CLOUDINARY_API_SECRET"),
+)
+
+
+def upload_image(image_path, folder_name):
+    with open(image_path, "rb") as image_file:
+        byte_array = bytearray(image_file.read())
+
+    response = upload(byte_array, folder=folder_name)
+
+    if response is None:
+        return None
+
+    return response["secure_url"]
+
+
+def get_files(folder):
+    folder_path = os.path.join("C:\\Users\\User\\Pictures\\terratreats\\", folder)
+    file_paths = []
+    for root, directories, files in os.walk(folder_path):
+        for filename in files:
+            file_paths.append(os.path.join(folder_path, filename))
+
+    return file_paths
+
+
+def populate_product(seller):
+    try:
+        with Session(engine) as session:
+            seller_id = seller["id"]
+            # update the seller's delivery schedule
+            for sched in seller["delivery"]:
+                delivery_query = DeliverySchedule(
+                    seller_id=seller_id,
+                    schedule=sched,
+                )
+                session.add(delivery_query)
+
+            file_paths = get_files(str(seller_id))
+
+            for prod in seller["products"]:
+                image_url = upload_image(file_paths[prod["image"]], seller_id)
+
+                prod_query = Product(
+                    product_name=prod["product_name"],
+                    description=prod["description"],
+                    price=prod["price"],
+                    stock=prod["stock"],
+                    unit=prod["unit"],
+                    image_url=image_url,
+                    category_id=prod["category_id"],
+                    shipping_fee=prod["shipping_fee"],
+                    seller_id=seller_id,
+                )
+
+                session.add(prod_query)
+
+            session.commit()
+    except Exception as e:
+        print(e)
+        session.rollback()
+
+
+def main():
+    for prod in products:
+        populate_product(prod)
+
+
+if __name__ == "__main__":
+    main()
