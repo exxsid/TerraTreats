49b3e556f75f7d1b76924f84e3feb818ceaec74b
exxsid <cortezleoanthony5@gmail.com>
2024-05-23 10:10:02 +0800

    to add other user

---
+++ server/api/populate_user.py
@@ -0,0 +1,384 @@
+consumers = [
+    {
+        "email": "resitovelasco@gmail.com",
+        "password": "nH[:w$9-",
+        "first_name": "Resito",
+        "last_name": "Velasco",
+        "phonenumber": "+638955230023",
+        "is_seller": False,
+        "is_verified": True,
+        "address": {
+            "street": "",
+            "barangay": "Bulala",
+            "city": "Bacnotan",
+            "province": "La Union",
+            "postal_code": "2515",
+            "latitude": 16.7294299,
+            "longitude": 120.3458003,
+        },
+    },
+    {
+        "email": "bagwishernandez@gmail.com",
+        "password": "Law%;3m]",
+        "first_name": "Bagwis",
+        "last_name": "Hernandez",
+        "phonenumber": "+639116886653",
+        "is_seller": False,
+        "is_verified": True,
+        "address": {
+            "street": "",
+            "barangay": "Taboc",
+            "city": "San Juan",
+            "province": "La Union",
+            "postal_code": "2514",
+            "latitude": 16.6949441,
+            "longitude": 120.3390979,
+        },
+    },
+    {
+        "email": "bernardorubilyn@gmail.com",
+        "password": "cnhT)5Ag",
+        "first_name": "Rubilyn",
+        "last_name": "Bernardo",
+        "phonenumber": "+638137620774",
+        "is_seller": False,
+        "is_verified": True,
+        "address": {
+            "street": "",
+            "barangay": "Sto. Rosario",
+            "city": "San Juan",
+            "province": "La Union",
+            "postal_code": "2514",
+            "latitude": 16.6808814,
+            "longitude": 120.3498498,
+        },
+    },
+    {
+        "email": "JesusaGutierrez@gmail.com",
+        "password": "av;%A2h?",
+        "first_name": "Jesusa",
+        "last_name": "Gutierrez",
+        "phonenumber": "+639185305976",
+        "is_seller": False,
+        "is_verified": True,
+        "address": {
+            "street": "",
+            "barangay": "Lingsat",
+            "city": "San Fernando City",
+            "province": "La Union",
+            "postal_code": "2500",
+            "latitude": 16.6474825,
+            "longitude": 120.3129567,
+        },
+    },
+    {
+        "email": "jovelynbautista@gmail.com",
+        "password": "gA@H[E6!",
+        "first_name": "Jovelyn",
+        "last_name": "Bautista",
+        "phonenumber": "+639116277904",
+        "is_seller": False,
+        "is_verified": True,
+        "address": {
+            "street": "",
+            "barangay": "Dalumpinas",
+            "city": "San Fernando City",
+            "province": "La Union",
+            "postal_code": "2500",
+            "latitude": 16.6461513,
+            "longitude": 120.3261935,
+        },
+    },
+]
+
+seller = [
+    {
+        "email": "leizelromero@gmail.com",
+        "password": "D2g4U_^&",
+        "first_name": "Leizel",
+        "last_name": "Romero",
+        "phonenumber": "+639091954721",
+        "is_seller": True,
+        "is_verified": True,
+        "address": {
+            "street": "",
+            "barangay": "Sipulo",
+            "city": "Bacnotan",
+            "province": "La Union",
+            "postal_code": "2515",
+            "latitude": 16.7343705,
+            "longitude": 120.3475531,
+        },
+        "seller": {
+            "description": "Explore a bountiful selection of farm-fresh fruits, vegetables, and more at Fresh Harvest Market. From juicy berries to crisp greens, we have everything you need to create healthy and delicious meals.",
+            "is_verified": True,
+        },
+    },
+    {
+        "email": "jamestala@gmail.com",
+        "password": "p3[bN~?S",
+        "first_name": "Tala",
+        "last_name": "James",
+        "phonenumber": "+639194851096",
+        "is_seller": True,
+        "is_verified": True,
+        "address": {
+            "street": "",
+            "barangay": "San Martin",
+            "city": "Bacnotan",
+            "province": "La Union",
+            "postal_code": "2515",
+            "latitude": 16.7342608,
+            "longitude": 120.3546481,
+        },
+        "seller": {
+            "description": "Discover the goodness of nature at Nature's Bounty Co. Our online store offers a wide variety of organic fruits, vegetables, and other natural products sourced directly from local farms.",
+            "is_verified": True,
+        },
+    },
+    {
+        "email": "LaarniSmith@gmail.com",
+        "password": "R;t9b(W@",
+        "first_name": "Laarni",
+        "last_name": "Smith",
+        "phonenumber": "+639088413277",
+        "is_seller": True,
+        "is_verified": True,
+        "address": {
+            "street": "",
+            "barangay": "Cabaroan",
+            "city": "Bacnotan",
+            "province": "La Union",
+            "postal_code": "2515",
+            "latitude": 16.738066,
+            "longitude": 120.3649674,
+        },
+        "seller": {
+            "description": "Shop for the freshest produce at Garden Fresh Grocers. With a focus on quality and sustainability, we bring you a diverse range of fruits, vegetables, and specialty items to elevate your culinary experience.",
+            "is_verified": True,
+        },
+    },
+    {
+        "email": "MarivicBautista@gmail.com",
+        "password": "r7&W$x>G",
+        "first_name": "Marivic",
+        "last_name": "Bautista",
+        "phonenumber": "+639062018615",
+        "is_seller": True,
+        "is_verified": True,
+        "address": {
+            "street": "",
+            "barangay": "Sta. Cruz",
+            "city": "Bacnotan",
+            "province": "La Union",
+            "postal_code": "2515",
+            "latitude": 16.7388241,
+            "longitude": 120.3446444,
+        },
+        "seller": {
+            "description": "Experience the farm-to-table difference with Farm to Table Emporium. Our curated selection of seasonal fruits, vegetables, and artisanal products ensures that you enjoy the best flavors nature has to offer.",
+            "is_verified": True,
+        },
+    },
+    {
+        "email": "cabreracherry@gmail.com",
+        "password": "Q6j;9~J^",
+        "first_name": "Cherry",
+        "last_name": "Cabrera",
+        "phonenumber": "+639055021752",
+        "is_seller": True,
+        "is_verified": True,
+        "address": {
+            "street": "",
+            "barangay": "Cabarsican",
+            "city": "Bacnotan",
+            "province": "La Union",
+            "postal_code": "2515",
+            "latitude": 16.7457431,
+            "longitude": 120.3411651,
+        },
+        "seller": {
+            "description": "Cultivate your love for fresh produce at Green Thumb Market. Browse through our selection of handpicked fruits, vegetables, and gourmet treats to add a touch of green goodness to your meals.",
+            "is_verified": True,
+        },
+    },
+    {
+        "email": "igme.jimenez@gmail.com",
+        "password": "cz8W%e(C",
+        "first_name": "Igme",
+        "last_name": "Jimenez",
+        "phonenumber": "+639127731493",
+        "is_seller": True,
+        "is_verified": True,
+        "address": {
+            "street": "",
+            "barangay": "Tammocaloa",
+            "city": "Bacnotan",
+            "province": "La Union",
+            "postal_code": "2515",
+            "latitude": 16.7481391,
+            "longitude": 120.3399035,
+        },
+        "seller": {
+            "description": "Step into a world of orchard-fresh delights at Orchard Oasis. From tree-ripened fruits to garden-fresh vegetables, our store is a paradise for food enthusiasts looking for quality and variety.",
+            "is_verified": True,
+        },
+    },
+    {
+        "email": "genergonzales@gmail.com",
+        "password": "t~3hx+FV",
+        "first_name": "Gener",
+        "last_name": "Gonzales",
+        "phonenumber": "+639425594080",
+        "is_seller": True,
+        "is_verified": True,
+        "address": {
+            "street": "",
+            "barangay": "Galongen",
+            "city": "Bacnotan",
+            "province": "La Union",
+            "postal_code": "2515",
+            "latitude": 16.7562936,
+            "longitude": 120.3400879,
+        },
+        "seller": {
+            "description": "Embrace a healthier lifestyle with Veggie Village. Our online store is a one-stop destination for premium fruits, vegetables, and plant-based products that cater to your nutritional needs and culinary preferences.",
+            "is_verified": True,
+        },
+    },
+    {
+        "email": "EdcelCristobal@gmail.com",
+        "password": "PnD#3y4g",
+        "first_name": "Edcel",
+        "last_name": "Cristobal",
+        "phonenumber": "+639811059852",
+        "is_seller": True,
+        "is_verified": True,
+        "address": {
+            "street": "",
+            "barangay": "Quirino",
+            "city": "Bacnotan",
+            "province": "La Union",
+            "postal_code": "2515",
+            "latitude": 16.7694117,
+            "longitude": 120.3330009,
+        },
+        "seller": {
+            "description": "Uncover a treasure trove of fresh finds at Fruitful Finds Co. Explore our collection of seasonal fruits, exotic vegetables, and gourmet ingredients that will inspire your next culinary masterpiece.",
+            "is_verified": True,
+        },
+    },
+    {
+        "email": "alfonsoreynante@gmail.com",
+        "password": "kp4Cw/_c",
+        "first_name": "Reynante",
+        "last_name": "Alfonso",
+        "phonenumber": "+639071643228",
+        "is_seller": True,
+        "is_verified": True,
+        "address": {
+            "street": "",
+            "barangay": "Ballogo",
+            "city": "Bacnotan",
+            "province": "La Union",
+            "postal_code": "2515",
+            "latitude": 16.7654381,
+            "longitude": 120.3433797,
+        },
+        "seller": {
+            "description": "Find your haven of fresh produce at Harvest Haven. With a focus on sustainability and flavor, we offer a diverse range of fruits, vegetables, and specialty items that celebrate the beauty of nature's bounty.",
+            "is_verified": True,
+        },
+    },
+    {
+        "email": "RizalSan@gmail.com",
+        "password": "Sa_7?^TE",
+        "first_name": "Rizal",
+        "last_name": "San",
+        "phonenumber": "+639573265782",
+        "is_seller": True,
+        "is_verified": True,
+        "address": {
+            "street": "",
+            "barangay": "Nagatiran",
+            "city": "Bacnotan",
+            "province": "La Union",
+            "postal_code": "2515",
+            "latitude": 16.7677498,
+            "longitude": 120.3559907,
+        },
+        "seller": {
+            "description": "Elevate your shopping experience with Market Fresh Express. Discover a wide array of farm-fresh fruits, garden vegetables, and specialty products that bring the essence of a local market right to your doorstep.",
+            "is_verified": True,
+        },
+    },
+]
+
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+
+from models.models import User, Address, Seller
+from models.api_base_model import Login, Signup
+
+engine = engine = create_engine(
+    "postgresql+psycopg://leo:1234@127.0.0.1:5432/terratreats", echo=True
+)
+
+
+def populate(user, is_seller: bool = False):
+    try:
+        with Session(engine) as session:
+            u = User(
+                email=user["email"],
+                password=user["password"],
+                first_name=user["first_name"],
+                last_name=user["last_name"],
+                phonenumber=user["phonenumber"],
+                is_seller=user["is_seller"],
+                is_verified=user["is_verified"],
+            )
+            session.add(u)
+            session.flush()
+
+            user_id = u.id
+
+            addr = user["address"]
+
+            address = Address(
+                user_id=user_id,
+                street=addr["street"],
+                barangay=addr["barangay"],
+                city=addr["city"],
+                province=addr["province"],
+                postal_code=addr["postal_code"],
+                latitude=addr["latitude"],
+                longitude=addr["longitude"],
+            )
+
+            session.add(address)
+
+            if is_seller:
+                s = user["seller"]
+                seller = Seller(
+                    user_id=user_id,
+                    description=s["description"],
+                    is_verified=s["is_verified"],
+                )
+
+                session.add(seller)
+
+            session.commit()
+    except Exception as e:
+        print("error", e)
+        session.rollback()
+
+
+def main():
+    for user in seller:
+        populate(user, True)
+
+
+if __name__ == "__main__":
+    print("start...")
+    main()
+    print("...done")
