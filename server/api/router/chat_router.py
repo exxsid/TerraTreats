fec007cce7cefc17383ea4a0285c56c612116050
exxsid <cortezleoanthony5@gmail.com>
2024-05-25 15:47:24 +0800

    fix new chat in getting chat history

--- server/api/router/chat_router.py
+++ server/api/router/chat_router.py
@@ -12,6 +12,7 @@ from utils import chat_util as cu
 
 router = APIRouter()
 
+
 class ConnectionManager:
     def __init__(self):
         self.connections: Dict[int, WebSocket] = {}
@@ -22,8 +23,7 @@ class ConnectionManager:
     async def connect(self, websocket: WebSocket, id: int):
         await websocket.accept()
         self.connections[id] = websocket
-        
-            
+
     async def handle_connections(self, websocket: WebSocket, id: int):
         try:
             while True:
@@ -35,35 +35,47 @@ class ConnectionManager:
         for conn in self.connections:
             if conn is not sender:
                 await conn.send_text(data)
-                
+
     async def private_message(self, chat: SendChat):
         # when the sender is not connected to the socket
         if not self.connections.get(chat.sender_id):
             raise Exception("No connected")
-        
+
         # when the recipient is offline
         if not self.connections.get(chat.recipient_id):
-            await self.connections.get(chat.sender_id).send_json(jsonable_encoder({
-                "sender_id": chat.sender_id,
-                "recipient_id": chat.recipient_id,
-                "message": chat.message,
-                "timestamp": datetime.now(),
-            }))
+            await self.connections.get(chat.sender_id).send_json(
+                jsonable_encoder(
+                    {
+                        "sender_id": chat.sender_id,
+                        "recipient_id": chat.recipient_id,
+                        "message": chat.message,
+                        "timestamp": datetime.now(),
+                    }
+                )
+            )
             await self.store_message(chat)
             return
 
-        await self.connections.get(chat.recipient_id).send_json(jsonable_encoder({
-            "sender_id": chat.sender_id,
-            "recipient_id": chat.recipient_id,
-            "message": chat.message,
-            "timestamp": datetime.now(),
-            }))
-        await self.connections.get(chat.sender_id).send_json(jsonable_encoder({
-            "sender_id": chat.sender_id,
-            "recipient_id": chat.recipient_id,
-            "message": chat.message,
-            "timestamp": datetime.now(),
-            }))
+        await self.connections.get(chat.recipient_id).send_json(
+            jsonable_encoder(
+                {
+                    "sender_id": chat.sender_id,
+                    "recipient_id": chat.recipient_id,
+                    "message": chat.message,
+                    "timestamp": datetime.now(),
+                }
+            )
+        )
+        await self.connections.get(chat.sender_id).send_json(
+            jsonable_encoder(
+                {
+                    "sender_id": chat.sender_id,
+                    "recipient_id": chat.recipient_id,
+                    "message": chat.message,
+                    "timestamp": datetime.now(),
+                }
+            )
+        )
         # await self.connections.get(chat.recipient_id).send_text(chat.message)
         await self.store_message(chat)
 
@@ -75,82 +87,94 @@ class ConnectionManager:
 
         # when the chat document is already exist
         if chat_doc:
-            chat_doc["messages"].append({
-                "sender_id": chat.sender_id, 
-                "recipient_id": chat.recipient_id, 
-                "message": chat.message, 
-                "timestamp": datetime.now()
-                })
-            
-            await self.chat.update_one({"_id": chat_doc["_id"]}, {"$set": {"messages": chat_doc["messages"]}})
+            chat_doc["messages"].append(
+                {
+                    "sender_id": chat.sender_id,
+                    "recipient_id": chat.recipient_id,
+                    "message": chat.message,
+                    "timestamp": datetime.now(),
+                }
+            )
+
+            await self.chat.update_one(
+                {"_id": chat_doc["_id"]}, {"$set": {"messages": chat_doc["messages"]}}
+            )
         else:
             chat_doc = {
                 "participants": [chat.sender_id, chat.recipient_id],
-                "messages": [{
-                    "sender_id": chat.sender_id, 
-                    "recipient_id": chat.recipient_id, 
-                    "message": chat.message, 
-                    "timestamp": datetime.now()
-                    }]
+                "messages": [
+                    {
+                        "sender_id": chat.sender_id,
+                        "recipient_id": chat.recipient_id,
+                        "message": chat.message,
+                        "timestamp": datetime.now(),
+                    }
+                ],
             }
             await self.chat.insert_one(chat_doc)
 
     async def chat_messages(self, user_id: int):
-        # to be more efficient, retrieving of chats include only 
+        # to be more efficient, retrieving of chats include only
         # _id and participants
-        filter = {
-                "_id": 1,
-                'participants': 1
-            }
-        chat_doc = await self.chat.find({'participants': user_id}, filter).to_list(length=None)
-        
+        filter = {"_id": 1, "participants": 1}
+        chat_doc = await self.chat.find({"participants": user_id}, filter).to_list(
+            length=None
+        )
+
         if not chat_doc:
             return []
-        
+
         result = []
         # get the recipient name
         for chat in chat_doc:
-            recipient_id = chat.get('participants')[0] if chat.get('participants')[0] != user_id else chat.get('participants')[1]
+            recipient_id = (
+                chat.get("participants")[0]
+                if chat.get("participants")[0] != user_id
+                else chat.get("participants")[1]
+            )
 
             name = await cu.get_user_name(recipient_id)
-            
+
             temp = {
-                    'id': str(chat.get('_id')),
-                    'recipient': name,
-                    'recipeint_id': recipient_id
-                }
+                "id": str(chat.get("_id")),
+                "recipient": name,
+                "recipeint_id": recipient_id,
+            }
             result.append(temp)
         return result
 
     async def chat_history(self, chat_id: str):
-        chat_doc = await self.chat.find_one({'_id': ObjectId(chat_id)})
+        chat_doc = await self.chat.find_one({"_id": ObjectId(chat_id)})
 
         if not chat_doc:
             return []
 
-        sorted_messages = sorted(chat_doc["messages"], key=lambda msg: msg["timestamp"], reverse=True)
+        sorted_messages = sorted(
+            chat_doc["messages"], key=lambda msg: msg["timestamp"], reverse=True
+        )
 
         return [
-                {
-                    "sender_id": msg["sender_id"],
-                    "message": msg["message"],
-                    "timestamp": msg["timestamp"]
-                }
-                for msg in sorted_messages
-            ]
-        
+            {
+                "sender_id": msg["sender_id"],
+                "message": msg["message"],
+                "timestamp": msg["timestamp"],
+            }
+            for msg in sorted_messages
+        ]
 
     def disconnect(self, websocket, id):
         self.connections.pop(id)
 
+
 manager = ConnectionManager()
+
 
 @router.websocket("/connect")
 async def connect(websocket: WebSocket, id: int):
     await manager.connect(websocket, id)
     await manager.handle_connections(websocket, id)
-    
-    
+
+
 # let the user to send chat to a specific user
 @router.post("/chat/send")
 async def private_chat(chat: SendChat):
@@ -158,18 +182,23 @@ async def private_chat(chat: SendChat):
         await manager.private_message(chat)
 
         return JSONResponse(
-            content=jsonable_encoder({'message': chat.message}), status_code=200)
+            content=jsonable_encoder({"message": chat.message}), status_code=200
+        )
     except Exception as e:
         print(e)
         return Response(status_code=400)
-    
-    
+
+
 # get the chat history for private message
 @router.get("/chat/history")
 async def get_chat_history(chat_id: str):
+    if len(chat_id) == 0:
+        return JSONResponse(content=jsonable_encoder([]), status_code=200)
+
     result = await manager.chat_history(chat_id)
 
     return JSONResponse(content=jsonable_encoder(result), status_code=200)
+
 
 # get the chat that the user is in
 @router.get("/chat/messages")
@@ -177,4 +206,3 @@ async def get_chats(id: int):
     result = await manager.chat_messages(id)
 
     return JSONResponse(content=jsonable_encoder(result), status_code=200)
-
